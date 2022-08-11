abef06acdef91f347093f0361c7b51dab2a4629c
Fábio <fabio_henrique_estevam@hotmail.com>
2022-08-11 20:47:27 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/estevam/listademateriais/ListaDeMateriaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.estevam.listademateriais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeMateriaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeMateriaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estevam/listademateriais/ListaDeMateriaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estevam.listademateriais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeMateriaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
